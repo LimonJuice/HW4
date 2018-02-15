e0f460c9e870e6a8fd34f7e543cbfafcd1186cee
Marco Lamanda <32776739+LimonJuice@users.noreply.github.com>
2018-02-15 19:30:25 +0100

    Update Nucletides_Game.py

--- Nucletides_Game.py
+++ Nucletides_Game.py
@@ -1,144 +1,114 @@
-Nucleotides = 'TCGGATCATGGTTACCCTGA'
-
-print('\nINSTRUCTIONS!' 
-      '\n At every turn a Player may delete either one or two Nucleotides from the sequence.'
-      '\n The Player who is left with a uni-nucleotide sequence of an arbitrary length '
-      '\n (i.e., the sequence containing only one of 4 possible Nucleotides),'
-      '\n loses the game.','\n')
+import random
 
 
-print('Sequence of Nucleotides Inserted: ',Nucleotides)
-
-#Requirements function defines the limit to play the game
-
-
-def Requirements(seq):
-    dic = {}
-    adenine = 0
-    thymine = 0
-    guanine = 0
-    cytosine = 0
-
-    for i in seq:
-        if i == 'A':          #converter of seq elements into 'requirements' points
-            adenine += 1
-
-        elif i == 'T':
-            thymine += 1
-
-        elif i == 'G':
-            guanine += 1
-
-        elif i == 'C':
-            cytosine += 1
+def start_game(a, t, c, g):
+    dict = {'A': a, 'T': t, 'C': c, 'G': g}
+    player1 = input("Please enter a name for player 1: ")
+    player2 = input("Please enter a name for player 2: ")
+    winner = ntide_game(dict, player1, player2)
+    return print ("Congratulations " +winner+ " you won!")
 
 
-    dic['A'] = adenine
-    dic['T'] = thymine
-    dic['G'] = guanine
-    dic['C'] = cytosine
+
+def ntide_game(n_dict, player1, player2="computer"):
+
+    ml = True  # move left
+    tc = 0  # turn count
+    done = False
+    print("game begins, follwing Nucleotides are set: ", n_dict)
+    # set player that begins
 
 
-    print(dic)
-    h = 0
-    for i in dic.values():
-        if i == 1:
-            h += 1
-            continue
+    if random.getrandbits(1) == False:
+        currplayer = player1
+        print (player1, " begins")
+    else:
+        currplayer = player2
+        print(player2, " begins")
 
+    while True:
+        # get move of player
+        if currplayer == "computer":
+            move = winning_strat(n_dict)
         else:
-            h = 0
+            print(n_dict)
+            print("total number of Nucleotides: "+ str(sum(n_dict.values())))
+            move = input(currplayer + " please enter your move (E.g.: a,2 or c,1) :")
 
-    if h == 4:
-        print('Game Over')
+        try:
+            n, v = move.split(",")
+        except ValueError:
+            if move == "" and tc == 1:
+                done = True
+                ml = False
+            else:
+                print("Invalid input please try again...")
+                continue
+        v = int(v)
+        n = n.upper()
 
-    else:
-        print('Ready to play!','Good Luck Have Fun')
+        if done != True:
+            if v > 2 or v < 0 or n not in n_dict.keys() or n_dict[n] == 0:
+                print("Invalid move found please try again...")
+                continue
+            if tc ==1 and v == 2:
+                print("Only 1 move possible...")
+                continue
+            elif v == 2:
+                ml = False
 
-
-Requirements(Nucleotides)
-
-
-def Check(seq):
-    l = len(seq)
-    print('# of N:',l)
-
-    if l % 4 == 3 and l > 4:
-        print('Tip: Player can win by picking One Nucleotide')
-
-    elif l % 4 == 1 and l > 4:
-        print('Tip: Player can win by picking Two Nucleotides')
-
-    elif l % 4 == 0 and l != 8 and l > 4:
-        print('Tip: Player can pick either One or Two Nucleotides.')
-
-    elif l < 4:
-        print('# of Nucleotides not sufficient!')
-        print ('Insert Nucleotides to Continue!')
-
-    else:
-        if l == 4 :
-            print ('You Lose!')
-
-
-Check(Nucleotides)
+                n_dict[n] = n_dict[n]-v
+            else:
+                n_dict[n] = n_dict[n] - v
+                tc += 1
+            if tc == 2:
+                ml = False
 
 
+        # check if somebody won (3 out of 4 Ntides are empty)
+        # if after my turn there are 3 out of 4 empty entries I won
+        zerolist = [zeroes for zeroes in n_dict.values() if zeroes == 0]
+        if len(zerolist) == 3:
+            return currplayer #return the winning player
 
-# schedule to follow in game phases
-
-
-def Strategies(seq_size):
-    for N_seq in range(seq_size, 4, -1):
-        f_move = 1
-        s_move = 2
-        print('If the size of the sequence is:', N_seq)
-
-        if N_seq % 4 == 1 and N_seq - 2 != 4 and N_seq != 7 and N_seq != 5:
-
-            print('The Player on the turn deletes Two Nucleotides -> takes', s_move,'\n')
-            # a Player can delete either two on one time or one on the other time
-
-            if  N_seq % 2 == 0:
-                N_seq -= 4
+        if ml == False:
+            if currplayer == player1:
+                print("Turn player ", currplayer, " over")
+                print(chr(27) + "[2J")
+                currplayer = player2
+                ml = True
+                done = False
+                tc = 0
 
             else:
-                N_seq -= 3
+                print("Turn player ", currplayer, " over")
+                print(chr(27) + "[2J")
+                currplayer = player1
+                ml = True
+                done = False
+                tc = 0
+def winning_strat(nnucl):
+    totnucl = sum(nnucl.values())
+    nrows = 4
+    for i in nnucl.values():
+        if i == 0:
+            nrows -= 1
+    if nrows == 4:
+        if
+            if (totnucl -1)%3 != 0:   #winning
+            #take one from the bggest row
+            else:     #losing
 
-            continue
+    if nrows == 3:
+        if (totnucl-2)%3 != 0:    #winning
 
-        elif N_seq - 2 == 4 and N_seq != 7:
-            print('The Player on the turn deletes Two Nucleotides to win > takes', s_move)
-            print('Now the size of the sequence is', N_seq - 2)
-            print('So the other Player, on the next round, loses the game.','\n')
+        else:      #losing
 
-        elif N_seq - 1 == 4 and N_seq != 7:
-            print('The Player on the turn deletes Two Nucleotides to win > takes', s_move)
-            print('Now the size of the sequence is', N_seq - 1)
-            print('So the other Player, on the next round, loses the game.','\n')
+    if nrows == 2:
+        if totnucl%3 != 0:       #winning
+            #i win
+        else:        #losing
+            return False
 
-        elif N_seq % 4 == 3 and N_seq != 7:
-
-            print('The Player on the turn deletes one Nucleotide > takes', f_move,'\n')
-            # a Player can delete either two on one time or one on the other time
-            if N_seq % 2 == 0:
-                N_seq -= 3
-            else:
-                N_seq -= 4
-            continue
-
-        elif N_seq % 4 == 0:
-            print('The Player on the turn is free to delete One or Two Nucleotides > takes', f_move, 'or', s_move)
-            c = N_seq - 1
-            c2 = N_seq - 2
-            # the other Player can delete either two on one time or one on the other time
-
-            if N_seq % 2 == 0:
-                N_seq -= 4
-
-        else:
-            print('Careful Player 2, you have the chance to win by thinking in the right term')
-
-
-
-Strategies(len(Nucleotides))
+if __name__ == '__main__':
+    start_game(1,1,1,1)
